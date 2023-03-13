3fb8a583024a3645c8fa58249735e7f5494b0081
imran <imran-hossain@vicdigit-technologies.com>
2023-03-13 14:42:41 +0600

    our story card

--- Components/Home/SpecialServices/OurStory.js
+++ Components/Home/SpecialServices/OurStory.js
@@ -22,7 +22,34 @@ const OurStory = () => {
           
           
               <div>
-                
+                <div className={styles.bgStory}>
+                    <div className={styles.textContainer}>
+
+
+                        <h1 className={styles.heading}>Our Story <span><Image src={underline} alt="" className='ml-32'></Image></span> </h1>
+                        <div className=''>
+                            <p className="mb-5">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Sit delectus cumque asperiores culpa omnis modi, aliquid dolores nesciunt nemo pariatur earum commodi quis praesentium fugiat voluptatibus
+                                repellat expedita facilis. Doloribus, animi culpa nesciunt suscipit repudiandae minima hic quaerat excepturi! Inventore.</p>
+                            <button className={styles.button}>READ MORE
+
+                                <Image src={rightArrow} alt='' className='pt-1 h-5 w-6' ></Image>
+                            </button>
+                            <Image src={halfCircle2} alt="" className={styles.halfCircle2} />
+                        </div>
+                    </div>
+                </div>
+
+                <div className={styles.happyCustomer}>
+                    <div>
+                        <h2>955+</h2>
+                        <h4>Happy Customer</h4>
+                    </div>
+                    <div>
+                        <h2>245+</h2>
+                        <h4>Happy Customer</h4>
+                    </div>
+                </div>
+
               </div>
            </div>
            
